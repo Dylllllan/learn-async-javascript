40fa9347061d8cd8f9ddb93ff88ed0f78064e9c7
Dylan <31136360+Dylllllan@users.noreply.github.com>
2023-02-23 19:54:32 +0000

    Add markdown library

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Import the main CSS stylesheet
 import "./style.scss";
 
+import { parse } from "marked";
+
 import editor from "./editor";
 
 async function init() {
@@ -10,6 +12,8 @@ async function init() {
     const script = await (await fetch("./lessons/templates/lesson1.js")).text();
     // Add the lesson template script to the editor and move the cursor to the start
     editor.setValue(script, -1);
+
+    document.getElementById("content").innerHTML = parse("## Example of using markdown\nThis is good for generating content.  \nEven with `code snippets`.\n```\nalert(\"Hello World!\");\nvar a = 0;\n```");
 }
 
 init();
